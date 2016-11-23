3f03c895ea773045465ddaae3afbcb7063cc74f4
dcamilleri <dorian@dcamilleri.com>
2016-11-23 11:28:13 +0100

    Use Ember.String.htmlSafe to avoid warnings in Ember#2.6.0

--- addon/helpers/i18n-n.js
+++ addon/helpers/i18n-n.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import gettext from 'ember-cli-gettext/gettext';
 
 export function getText(params, hash) {
-  return new Ember.Handlebars.SafeString(gettext._n(...params, hash));
+  return new Ember.String.htmlSafe(gettext._n(...params, hash));
 }
 
 export default Ember.Helper.helper(getText);
